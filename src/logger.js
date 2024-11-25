eaf492c7ac021d71dc830e81f51334db906853ee
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-11-25 09:25:45 +0700

    docs: add logger documentation

--- src/logger.js
+++ src/logger.js
@@ -15,16 +15,26 @@ const pinoConfig = pino.pino({
 
 /**
  * @typedef {Object} LoggerContext
- * @property {string} requestId
- * @property {string} userId
- * @param {'info' | 'error' | 'warn'} level
- * @param {string} message
+ * @property {string} requestId - additional context
+ * @property {string} userId - additional context, so we can know who did what in our system
+ * @param {'info' | 'error' | 'warn'} level - log level
+ * @param {string} message - log message
  * @param {string} operationId - name of function. e.g createUserHandler, udpateUserHandler 
  * @param {LoggerContext | null} context
+ *
+ * Pino logger, it's mostly use to log error event to stdout.
+ * But we can also use it to log any important event
+ * so we can know who that did what in our system better
  */
 function logger(level, message, operationId, context) {
 	if (context) {
-		pinoConfig[level]({ message, operationId, context })
+		pinoConfig[level]({
+			message, context: {
+				requestId: context.requestId,
+				userId: context.userId,
+				operationId: operationId
+			}
+		})
 	}
 	pinoConfig[level]({ message, operationId })
 }
