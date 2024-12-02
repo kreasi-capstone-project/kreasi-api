120cfea6a31a1554d4135fcd0b9adc09afcbc8cd
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-12-02 21:17:14 +0700

    feat(validation): handling input validation error

--- src/server.js
+++ src/server.js
@@ -39,14 +39,31 @@ const init = async () => {
 	// Boom error handling to customize error response based on API Design
 	server.ext("onPreResponse", (request, h) => {
 		const response = request.response
-		console.log(response)
 		if (!response.isBoom || !(response instanceof Error)) {
 			return h.continue
 		}
-		const statusCode = response.isBoom ? response.output.payload.statusCode : 500;
 		const message = response.isBoom
 			? response.output.payload.message
 			: response.message || "An unexpected error occurred";
+		const statusCode = response.isBoom ? response.output.payload.statusCode : 500;
+
+		// check if error is come from Joi (validation error)
+		if (response.isBoom && response.output.payload.error === 'Bad Request' && response.details) {
+			// Transform Joi error details into an object with [field_name]: error message
+			const joiErrors = response.details.reduce((acc, detail) => {
+				const field = detail.path.join('.'); // Convert Joi path to string
+				acc[field] = detail.message.replace(/"([^"]+)"/, '$1');
+				return acc;
+			}, {});
+			return h.response({
+				status: 'fail',
+				errors: {
+					code: statusCode,
+					message: 'Validation error',
+					details: joiErrors // Include detailed validation errors
+				}
+			}).code(statusCode).takeover();
+		}
 
 		logger("error", message, "preResponse hook",
 			{
